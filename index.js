e281169c4e584bf41062dbcf110ba628c7052e35
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-20 23:52:18 +0200

    summary

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-fetch('https://google.com')
+function fn1() {
+    const url = 'http://website.com/api/';
+
+    return fn2(url)
+        .then(fn3)
+        .then(names => {
+            console.log(names);
+        })
+        .catch(err => console.log('Error message #2'));
+}
+
+function fn2(url) {
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => { return data })
+        .catch(err => console.log('Error message'));
+}
+
+function fn3(data) {
+    return data.map(element => element.name)
+}
